72ffe4646b3af7de022b7c703cdf51510a97c6b4
Eric-Cortez <ecrcortez@gmail.com>
2022-11-14 21:45:46 -0800

    datatype review

---
+++ python/datatypes.py
@@ -0,0 +1,16 @@
+# Python Data Types
+# python has the following data types built-in by default, in these categories:
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# NoneType:	None
+
+# Getting the data type
+# you can get the data type of any object by using the type() function
+x = 5
+print(type(x)) # <class 'int'>
