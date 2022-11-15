daeb2a8c05025a3ca16b0050f0031d5dff23e981
Eric-Cortez <ecrcortez@gmail.com>
2022-11-14 20:22:57 -0800

    python syntax

---
+++ python/syntax.py
@@ -0,0 +1,19 @@
+# This is a comment in python
+#  In python a comment is denoted by a # symbol
+
+# Python print statement
+print("Hello World")
+
+# Use command to check python installation
+# python --version
+
+# Use command to run python script Exmple
+#  python syntax.py
+
+#  to open a repl in the terminal use command `python` and `exit()` 
+# to exit the repl
+
+# if statement
+# Python uses indentation to define code blocks, instead of brackets.
+# if 5 > 2:
+#     print ("Five is greater than two!")
