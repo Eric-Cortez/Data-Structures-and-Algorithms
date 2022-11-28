77523a0168d739d583542c20b618b6afc7ec0db4
Eric-Cortez <ecrcortez@gmail.com>
2022-11-27 19:47:27 -0800

    scrum framework

--- appAcademyOpen/student-handbook/scrum.js
+++ appAcademyOpen/student-handbook/scrum.js
@@ -7,13 +7,22 @@
  */
 // Scrum is a framework that teams can use to build a sustainable product - development process.The framework defines
 
-// The product owner as the person that directs the highest priority development of the product
-// The Scrum master that helps the team follow its process and be most efficient
-// The development team that completes the work that the product owner specifies
-// The sprint, some amount of time during which the Scrum process is run
-// Sprint planning which provides the road map for what to accomplish during a sprint
-// The daily scrum, a meeting that the Scrum team attends to communicate its activities and blockers
-// The show and tell which allows a larger group visibility into the products of the sprint
-// The retrospective, which allows the team to determine process improvements
-// Backlog grooming performed by the product owner to ensure that the backlog of stories is well ordered and defined
+// 1) The product owner as the person that directs the highest priority development of the product
+
+// 2) The Scrum master that helps the team follow its process and be most efficient
+
+// 3) The development team that completes the work that the product owner specifies
+
+// 4) The sprint, some amount of time during which the Scrum process is run
+
+// 5) Sprint planning which provides the road map for what to accomplish during a sprint
+
+// 6) The daily scrum, a meeting that the Scrum team attends to communicate its activities and blockers
+
+// 7) The show and tell which allows a larger group visibility into the products of the sprint
+
+// 8) The retrospective, which allows the team to determine process improvements
+
+// 9) Backlog grooming performed by the product owner to ensure that the backlog of stories is well ordered and defined
+
 // That's the framework of Scrum, the most popular Agile software development methodology in use today.
