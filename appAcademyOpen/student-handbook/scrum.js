7cfaa265774bb09c0132b1437f04c2b8d38622ab
Eric-Cortez <ecrcortez@gmail.com>
2022-11-27 19:45:01 -0800

    scrum notes

--- appAcademyOpen/student-handbook/scrum.js
+++ appAcademyOpen/student-handbook/scrum.js
@@ -5,3 +5,15 @@
  * Jeff Sutherland invented the "Scrum process" in 1993 for use on a software development team
  * 
  */
+// Scrum is a framework that teams can use to build a sustainable product - development process.The framework defines
+
+// The product owner as the person that directs the highest priority development of the product
+// The Scrum master that helps the team follow its process and be most efficient
+// The development team that completes the work that the product owner specifies
+// The sprint, some amount of time during which the Scrum process is run
+// Sprint planning which provides the road map for what to accomplish during a sprint
+// The daily scrum, a meeting that the Scrum team attends to communicate its activities and blockers
+// The show and tell which allows a larger group visibility into the products of the sprint
+// The retrospective, which allows the team to determine process improvements
+// Backlog grooming performed by the product owner to ensure that the backlog of stories is well ordered and defined
+// That's the framework of Scrum, the most popular Agile software development methodology in use today.
